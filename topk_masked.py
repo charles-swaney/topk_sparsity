42b9349dcccf102ae15acac73dcb4a30e0209ea4
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-02-29 14:00:14 -0500

    refactor

--- topk_masked.py
+++ topk_masked.py
@@ -3,52 +3,48 @@ import torch.nn as nn
 import torchvision
 import torchvision.models as models
 import torch.nn.functional as F
+from torchvision.models.vision_transformer import VisionTransformer
 
-class CifarViT(models.vision_transformer.vit_b_16):
-    '''
-    vanilla vision transformer with output dimension=10
-    '''
-    def __init__(self, pretrained=False, num_classes=10):
-        super(CifarViT, self).__init__(pretrained=pretrained)
 
-        self.heads = nn.Linear(self.heads[0].in_features, num_classes)
-
-def top_k_mask(matrix, k):
+class CifarTopkViT(nn.Module):
     '''
-    Inputs:
-        matrix: the matrix to apply the mask to
-        k: number of elements in each row (corresponding to input vectors) to be nonzero
-    Outputs:
-        masked: masked version of matrix with only top k within each row nonzero
+    ViT for CIFAR10
     '''
-    _, indices = torch.topk(matrix, k, -1)
-    mask = torch.zeros_like(matrix)
-    mask.scatter_(-1, indices, 1)
-    masked = matrix * mask
-    return masked
+    def __init__(self,  k_value, num_layers=12):
+        super(CifarTopkViT, self).__init__()
+        self.device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
+        self.model = VisionTransformer(image_size=32, patch_size=4, num_layers=num_layers, num_heads=12, hidden_dim=768, mlp_dim=3072,
+                                       dropout=.5, attention_dropout=.5, num_classes=10)
+        self.k_value = k_value
+        for i in range(num_layers):
+            encoder_layer = self.model.encoder.layers[i]
+            mlp_layer = encoder_layer.mlp
+            for name, module in mlp_layer.named_children():
+                if isinstance(module, nn.GELU):
+                    setattr(mlp_layer, name, TopkGELU(self.k_value))
+    def forward(self, x):
+        x = x.to(self.device)
+        return self.model(x)
 
 class TopkGELU(nn.Module):
     def __init__(self, k):
         super(TopkGELU, self).__init__()
         self.k = k
-    def forward(self, x):
-        x = top_k_mask(F.gelu(x), self.k)
-        return x
-    
-class TopkViT():
-    '''
-    k_value: the k in Top-k
-    '''
-    def __init__(self, model, k_value):
-        self.device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
-        self.model = model
-        self.k_value = k_value
-        for i in range(len(12)):
-            layer_name = f'encoder_layer_{i}'
-            gelu_module = getattr(getattr(self.model.encoder.layers, layer_name), '1')
-            setattr(getattr(self.model.encoder.layers, layer_name), '1', TopkGELU(self.k_value))
+
+    def top_k_mask(self, matrix, k):
+        '''
+        Inputs:
+            matrix: the matrix to apply the mask to
+            k: number of elements in each row (corresponding to input vectors) to be nonzero
+        Outputs:
+            masked: masked version of matrix with only top k within each row nonzero
+      '''
+        _, indices = torch.topk(matrix, k, -1)
+        mask = torch.zeros_like(matrix)
+        mask.scatter_(-1, indices, 1)
+        masked = matrix * mask
+        return masked
     
     def forward(self, x):
-        x = x.to(self.device)
-        x = self.model(x)
+        x = self.top_k_mask(F.gelu(x), self.k)
         return x
