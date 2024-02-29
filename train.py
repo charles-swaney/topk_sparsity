6341f9a592ff384046bbdf5edf002bee449f80c5
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-02-29 00:40:17 -0500

    save lr and acc

--- train.py
+++ train.py
@@ -2,21 +2,26 @@ import torch
 import numpy as np
 import torch.optim as optim
 import time
+import os 
 
-def save_checkpoint(model, optimizer, epoch, time_taken, loss, filepath='checkpoint.pth'):
+def save_checkpoint(model, optimizer, epoch, time_taken, loss, accuracy, lr, dir='models', filename='checkpoint.pth'):
+    filepath = os.path.join(dir, filename)
     checkpoint = {
         'epoch': epoch,
         'time_taken': time_taken,
         'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
         'loss': loss,
+        'accuracy': accuracy,
+        'learning_rate': lr
     }
+    os.makedirs(dir, exist_ok=True)
     torch.save(checkpoint, filepath)
 
 def train(model, dataloader, num_epochs, criterion, optimizer, device='cuda'):
     losses = []
     trainAcc = []
-    model = model.to(device)
+    model = model.to(device).train()
     t1 = time.time()
 
     for epoch in range(num_epochs):
@@ -33,14 +38,18 @@ def train(model, dataloader, num_epochs, criterion, optimizer, device='cuda'):
             preds = torch.argmax(output, dim=1)
             num_correct = (preds == y).sum()
             num_samples = preds.size(0)
-            trainAcc.append(num_correct / num_samples)
+            accuracy = num_correct / num_samples
+            trainAcc.append(accuracy)
             losses.append(loss.detach().cpu().numpy())
+            current_learning_rate = optimizer.param_groups[0]['lr']
 
         if epoch % 20 == 0 and epoch > 0:
             t2 = time.time()
             elapsed = t2 - t1
             if epoch < 200:
-                save_checkpoint(model, optimizer, epoch, elapsed, loss, filepath=f'checkpoint_epoch_{epoch}.pth')
+                save_checkpoint(model, optimizer, epoch, elapsed, loss, accuracy,
+                                    current_learning_rate, dir='models', filename=f'checkpoint_epoch_{epoch}.pth')
             else:
-                save_checkpoint(model, optimizer, epoch, elapsed, loss, filepath=f'final_checkpoint.pth')
+                save_checkpoint(model, optimizer, epoch, elapsed, loss, accuracy, 
+                                    current_learning_rate, dir='models', filename=f'final_checkpoint.pth')
 
