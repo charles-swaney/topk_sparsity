e8fbf0b18d2aaeccf571e786dfccd6e13f2e9b46
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-03-06 02:00:02 -0500

    add test set evaluation before saving state dicts

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import torch
 import time
 import os 
 
+test_interval = 10
+
 def save_checkpoint(model, optimizer, epoch, time_taken, loss, accuracy, lr, dir='models', filename='checkpoint.pth'):
     filepath = os.path.join(dir, filename)
     checkpoint = {
@@ -16,14 +18,15 @@ def save_checkpoint(model, optimizer, epoch, time_taken, loss, accuracy, lr, dir
     os.makedirs(dir, exist_ok=True)
     torch.save(checkpoint, filepath)
 
-def train(model, dataloader, num_epochs, criterion, optimizer, scheduler, device='cuda'):
+def train(model, train_dataloader, test_dataloader, num_epochs, criterion, optimizer, scheduler, device='cuda'):
     losses = []
     trainAcc = []
     model = model.to(device).train()
     t1 = time.time()
+    best_loss = 10 ** 4
 
     for epoch in range(num_epochs):
-        for x, y in dataloader:
+        for x, y in train_dataloader:
             x, y = x.to(device), y.to(device)
 
             output = model(x)
@@ -43,13 +46,37 @@ def train(model, dataloader, num_epochs, criterion, optimizer, scheduler, device
         
         scheduler.step()
 
-        if epoch % 20 == 0 and epoch > 0:
+        if epoch % test_interval == 0 and epoch > 0:
+
             t2 = time.time()
             elapsed = t2 - t1
-            if epoch < 200:
-                save_checkpoint(model, optimizer, epoch, elapsed, loss, accuracy,
-                                    current_learning_rate, dir='models', filename=f'checkpoint_epoch_{epoch}.pth')
-            else:
-                save_checkpoint(model, optimizer, epoch, elapsed, loss, accuracy, 
-                                    current_learning_rate, dir='models', filename=f'final.pth')
 
+            current_loss = evaluate(model=model, dataloader=test_dataloader, criterion=criterion, device='cuda')
+
+            if current_loss < best_loss:
+                best_loss = current_loss
+                save_checkpoint(model, optimizer, epoch, elapsed, loss.item(), accuracy, current_learning_rate, dir='models',
+                                filename=f'checkpoint_epoch_{epoch}.pth')
+
+def evaluate(model, dataloader, criterion, device='cuda'):
+    model = model.to(device).eval()
+    test_losses = []
+    num_correct, test_total = 0, 0
+
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+
+            output = model(x)
+            loss = criterion(output, y)
+            test_losses.append(loss.item())
+
+            preds = torch.argmax(output, dim=1)
+            num_correct += (preds == y).sum().item()
+            test_total += preds.size(0)
+
+    test_accuracy = num_correct / test_total
+    mean_loss = sum(test_losses) / len(test_losses)
+
+    print(f'Test Loss: {mean_loss}, Test Accuracy: {test_accuracy}')
+    return mean_loss
