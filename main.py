c5d585e935c4d768dd1861e9cdc676ed6474347e
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-03-14 22:59:05 -0400

    finish main script

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
 import yaml
 import argparse
+import logging
 
-import copy
-import torch
-import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import torch.nn.init as init
+from torchvision.transforms import v2
 
-from torchvision.models.vision_transformer import VisionTransformer
-from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import default_collate
 
-import utils.dataloader as dataloader
-from models.topk_vit import TopkViT, TopkGeLU, init_weights
-from models.train import train, save_checkpoint, evaluate
+from utils.dataloader import load_cifar10
+from models.train import train
 from utils.scheduler import CosineAnnealingWarmupRestarts
 from models.model_init import initialize_model
 
-# need to applyCutMix [42], Mixup [43], Auto Augment [6], Repeated Augment [7] to all models
-
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
 
 def main():
-    parser = argparse.ArgumentParser(description="Train a ViT with Top-k masking.")
-    parser.add_argument('--config', type=str, default='config.yaml', help='Path to config file.')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    parser = argparse.ArgumentParser(description="Train a specified model using configuration from config.yaml.")
+    parser.add_argument('--model_name', type=str, required=True, help="Model's name as specified in config.yaml")
     args = parser.parse_args()
 
-    with open(args.config, 'r') as file:
+    with open('config.yaml', 'r') as file:
         config = yaml.safe_load(file)
 
-    train_loader = dataloader.load_cifar10(root='./data', train=True, download=True, batch_size=128, num_workers=4)
-    test_loader = dataloader.load_cifar10(root='./data', train=False, download=True, batch_size=512, num_workers=4)
+    model_config = config['models'][args.model_name]
 
-    for model in models:
-        optimizer = optim.Adam(model.parameters(), lr=initial_lr, weight_decay=weight_decay)
-        scheduler = CosineAnnealingWarmupRestarts(optimizer=optimizer, first_cycle_steps=warmup)
-        train(model, train_loader, test_loader, num_epochs, criterion, optimizer, scheduler, test_interval, device)
+    cutmix = v2.CutMix(num_classes=config['num_classes'])
+    mixup = v2.MixUp(num_classes=config['num_classes'])
+    cutmix_or_mixup = v2.RandomChoice([cutmix, mixup])
+
+    def collate_fn(batch):
+        return cutmix_or_mixup(*default_collate(batch))
+
+    train_loader = load_cifar10(root='./data', config=config, train=True, download=True, collate_fn=collate_fn)
+    test_loader = load_cifar10(root='./data', config=config, train=False, download=True)
+
+    model = initialize_model(model_config).to(config['device'])
+
+    optimizer = optim.Adam(model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
+    scheduler = CosineAnnealingWarmupRestarts(optimizer=optimizer, first_cycle_steps=config['warmup'])
+    train(model, config, train_loader, test_loader, scheduler, device=config['device'])
 
 if __name__ == '__main__':
     main()
