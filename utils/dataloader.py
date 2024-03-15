5dd5ae20a9a8915a3845d31cce1e26c3c520cc6b
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-03-14 22:58:58 -0400

    implement cutmix, mixup

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,46 +1,28 @@
-import os
-import subprocess
-import tarfile
-import pickle
-import torch
-from torchvision import datasets, transforms
-from torchvision.transforms import v2
+from torchvision import datasets
+from torchvision.transforms import v2, autoaugment
+from torch.utils.data import DataLoader
 
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-def download_cifar10(root='./data'):
-
-    os.makedirs(root, exist_ok=True)
-    cifar10_url = "http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz"    
-    tar_filepath = os.path.join(root, "cifar-10-binary.tar.gz")
-    subprocess.run(["wget", cifar10_url, "-O", tar_filepath, "-q"])
-    with tarfile.open(tar_filepath, "r:gz") as tar:
-        tar.extractall(path=root)
-
-    os.remove(tar_filepath)
-
-def load_cifar10(root='./data', train=True, download=True, batch_size=128, num_workers=4):
-    if download:
-        download_cifar10(root)
-    train_transform = transforms.Compose([
-        #transforms.RandomHorizontalFlip(),
-        #transforms.RandomRotation(degrees=15),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
+def load_cifar10(root='./data', config='config.yaml', train=True, download=True, collate_fn=None):
+    train_batch_size = config['train_batch_size']
+    test_batch_size = config['test_batch_size']
+    num_workers = config['num_workers']
+    train_transform = v2.Compose([
+        autoaugment.AutoAugment(),
+        v2.ToTensor(),
+        v2.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
     ])
-    test_transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
+    test_transform = v2.Compose([
+        v2.ToTensor(),
+        v2.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
     ])
     if train:
         dataset = datasets.CIFAR10(root=root, train=train, download=download, transform=train_transform)
+        dataloader = DataLoader(dataset, batch_size=train_batch_size, shuffle=True, num_workers=num_workers, collate_fn=collate_fn)
+
     else:
         dataset = datasets.CIFAR10(root=root, train=train, download=download, transform=test_transform)
-
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=train, num_workers=num_workers)
+        dataloader = DataLoader(dataset, batch_size=test_batch_size, shuffle=False, num_workers=num_workers)
+        
     return dataloader
 
 
