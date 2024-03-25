ebc66c6509785389a4595394c308af1aba63d8cc
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-03-25 16:05:00 -0400

    refactor, add logging

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,29 +1,66 @@
+import logging
+
 from torchvision import datasets
 from torchvision.transforms import v2, autoaugment
 from torch.utils.data import DataLoader
 
-def load_cifar10(root='./data', config='config.yaml', train=True, download=True, collate_fn=None):
+
+def load_cifar10(config, root='./data', train=True, download=True, **kwargs):
+    if config is None:
+        raise ValueError('No config.')
+
     train_batch_size = config['train_batch_size']
     test_batch_size = config['test_batch_size']
     num_workers = config['num_workers']
+
     train_transform = v2.Compose([
         autoaugment.AutoAugment(),
         v2.ToTensor(),
         v2.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
     ])
+
     test_transform = v2.Compose([
         v2.ToTensor(),
         v2.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])
     ])
+
+    logging.info(f"Loading CIFAR-10 dataset. Train: {train}, Download: {download}")
+
     if train:
-        dataset = datasets.CIFAR10(root=root, train=train, download=download, transform=train_transform)
-        dataloader = DataLoader(dataset, batch_size=train_batch_size, shuffle=True, num_workers=num_workers, collate_fn=collate_fn)
+        dataset = datasets.CIFAR10(
+            root=root,
+            train=train,
+            download=download,
+            transform=train_transform
+        )
+        try:
+            dataloader = DataLoader(
+                dataset,
+                batch_size=train_batch_size,
+                shuffle=True,
+                num_workers=num_workers
+            )
+        except Exception as e:
+            logging.error(f"Failed to create DataLoader: {e}")
+            return None
 
     else:
-        dataset = datasets.CIFAR10(root=root, train=train, download=download, transform=test_transform)
-        dataloader = DataLoader(dataset, batch_size=test_batch_size, shuffle=False, num_workers=num_workers)
+        dataset = datasets.CIFAR10(
+            root=root,
+            train=train,
+            download=download,
+            transform=test_transform
+        )
+        try:
+            dataloader = DataLoader(
+                dataset,
+                batch_size=test_batch_size,
+                shuffle=False,
+                num_workers=num_workers
+            )
+        except Exception as e:
+            logging.error(f"Failed to create DataLoader: {e}")
+            return None
         
+    logging.info("Successfully created DataLoader.")
     return dataloader
-
-
-    
--- utils/scheduler.py
+++ utils/scheduler.py
@@ -91,17 +91,6 @@ def build_scheduler(args, optimizer, n_iter_per_epoch):
     num_steps = int(args.epochs * n_iter_per_epoch)
     warmup_steps = int(args.warmup * n_iter_per_epoch)
 
-    # lr_scheduler = CosineLRScheduler(
-    #     optimizer,
-    #     t_initial=num_steps,
-    #     t_mul=1.,
-    #     lr_min=1e-5,
-    #     warmup_lr_init=1e-6,
-    #     warmup_t=warmup_steps,
-    #     cycle_limit=1,
-    #     t_in_epochs=False,
-    #     )
-
     lr_scheduler = CosineAnnealingWarmupRestarts(
         optimizer,
         first_cycle_steps=num_steps,
