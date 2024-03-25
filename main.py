d09cfa67f94a0f0929608ad84cbb96e0c7bb0214
Charles Swaney <67131291+charles-swaney@users.noreply.github.com>
2024-03-25 16:06:51 -0400

    add more logging, refactor

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import yaml
 import argparse
 import logging
+import sys
 
 import torch.optim as optim
-from torchvision.transforms import v2
 
 from utils.dataloader import load_cifar10
 from models.train import train
@@ -13,24 +13,63 @@ from models.model_init import initialize_model
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    parser = argparse.ArgumentParser(description="Train a specified model using configuration from config.yaml.")
-    parser.add_argument('--model_name', type=str, required=True, help="Model's name as specified in config.yaml")
+    parser = argparse.ArgumentParser(
+        description="Train a specified model using configuration from config.yaml.")
+    parser.add_argument(
+        '--model_name',
+        type=str,
+        required=True,
+        help="Model's name as specified in config.yaml"
+    )
     args = parser.parse_args()
 
-    with open('config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
+    try:
+        with open('config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
+    except FileNotFoundError:
+        logging.error("'config.yaml' file not found.")
+        sys.exit(1)
+    except yaml.YAMLError as e:
+        logging.error(f"Error parsing config file: {e}")
+        sys.exit(1)
 
-    model_config = config['models'][args.model_name]
+    try:
+        model_config = config['models'][args.model_name]
+    except KeyError:
+        logging.error(f"Model name '{args.model_name}' not found in 'config.yaml'.")
+        sys.exit(1)
 
-    train_loader = load_cifar10(root='./data', config=config, train=True, download=True)
-    test_loader = load_cifar10(root='./data', config=config, train=False, download=True)
+    train_loader = load_cifar10(
+        root='./data',
+        config=config,
+        train=True,
+        download=True,
+        persistent_workers=True
+    )
+
+    test_loader = load_cifar10(
+        root='./data',
+        config=config,
+        train=False,
+        download=True,
+        persistent_workers=True
+    )
 
     model = initialize_model(model_config).to(config['device'])
 
-    optimizer = optim.Adam(model.parameters(), lr=config['lr'], weight_decay=config['weight_decay'])
-    scheduler = CosineAnnealingWarmupRestarts(optimizer=optimizer, 
-                                              first_cycle_steps=config['first_cycle_steps'], warmup_steps=config['warmup_steps'])
-    train(model, config, train_loader, test_loader, scheduler, device=config['device'])
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=config['lr'],
+        weight_decay=config['weight_decay']
+    )
+    scheduler = CosineAnnealingWarmupRestarts(
+        optimizer=optimizer,
+        first_cycle_steps=config['first_cycle_steps'],
+        warmup_steps=config['warmup_steps']
+    )
+
+    train(config, model, train_loader, test_loader, scheduler, device=config['device'])
+
 
 if __name__ == '__main__':
     main()
